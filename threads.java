f112247a369699c0ff9556c9d7d4a1d0d564091a
Shashikirana <126816648+Shashikiran1297@users.noreply.github.com>
2023-07-26 00:05:12 +0530

    Update threads.java

--- threads.java
+++ threads.java
@@ -1,1 +1,48 @@
+package threads;
+
+class ABC extends Thread{
+	public void run() {
+		for(int i=1;i<100;i++) {
+			System.out.println("shashi");
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
+class BCD extends Thread{
+	public void run() {
+		for(int i=1;i<100;i++) {
+			System.out.println("kiran");
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
+public class basicMultiThreading {
+
+	public static void main(String[] args) {
+		ABC A=new ABC();
+		BCD B=new BCD();
+		
+//		A.setPriority(10);
+//		B.setPriority(3);
+		
+		A.setPriority(Thread.MAX_PRIORITY-1);
+		B.setPriority(Thread.MIN_PRIORITY+3);
+		
+		//MAX_PRIORITY=10,MIN_PRIORITY=1,NORM_PRIORITY=5
+		
+		System.out.println(A.getPriority());
+		System.out.println(B.getPriority());
+		
+		A.start();
+		B.start();
+	}
+}
 
